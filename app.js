5ff0cb32da42891fa6a1f0d99d716e72687f4eb8
Wesley Henrique Francisco <wesley.hf93@hotmail.com>
2016-02-24 18:01:41 -0300

    Yahoo finance added

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var request = require('request');
 
 var app = express();
 var port = process.env.PORT || 1337;
+// var apiUrl = 'http://developers.agenciaideias.com.br/cotacoes/json';
+var apiUrl = 'http://finance.yahoo.com/d/quotes.csv?e=.csv&f=sl1d1t1&s=USDBRL=X';
 
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -16,13 +19,32 @@ app.listen(port, function() {
 
 app.post('/hello', function(req, res, next){
     var userName = req.body.user_name;
-    var botPayload = {
-        text: 'Hello ' + userName + ', fuck you.',
-    };
+    var dolar = '0.0';
+    var text = '';
 
-    if (userName !== 'slackbot') {
-        return res.status(200).json(botPayload);
-    } else {
-        return res.status(200).end();
-    }
+    request({
+        url: apiUrl,
+        json: true
+    }, function(error, response, body){
+        if (!error && response.statusCode === 200) {
+            var value = body.split(',');
+            dolar = value[1];
+
+            if (userName == 'dbpolito') {
+                text = 'Hey there, boss! Eager to get your hands on some dollars, eh? Alright, the USD exchange rate to BRL today is ' + dolar + '. One day I will be as rich as you are!';
+            } else {
+                text = 'Hello there, fellow capitalist ' + userName + '! As far as my stock trader contact tells me, today the USD exchange rate to BRL is ' + dolar + ' !';
+            }
+
+            var botPayload = {
+                text: text,
+            };
+
+            if (userName !== 'slackbot') {
+                return res.status(200).json(botPayload);
+            } else {
+                return res.status(200).end();
+            }
+        }
+    });
 });
