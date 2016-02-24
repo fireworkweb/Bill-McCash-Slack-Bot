0e289aada278d4da3c4c3597d798b7323f9f5a73
Wesley Henrique Francisco <wesley.hf93@hotmail.com>
2016-02-24 19:10:24 -0300

    Bill McCash Slack Bot

--- app.js
+++ app.js
@@ -4,47 +4,52 @@ var request = require('request');
 
 var app = express();
 var port = process.env.PORT || 1337;
-// var apiUrl = 'http://developers.agenciaideias.com.br/cotacoes/json';
-var apiUrl = 'http://finance.yahoo.com/d/quotes.csv?e=.csv&f=sl1d1t1&s=USDBRL=X';
+var currency = {
+    from: 'USD',
+    to: 'BRL',
+};
+
+var apiUrl = 'http://finance.yahoo.com/d/quotes.csv?e=.csv&f=sl1d1t1&s=' + currency.from + currency.to + '=X';
 
 app.use(bodyParser.urlencoded({extended: true}));
 
+// index
 app.get('/', function(req, res) {
-    res.status(200).send('Hello World');
+    res.status(200).send('Bill McCash Slack Bot is running.');
 });
 
 app.listen(port, function() {
-    console.log('Listening on port ' + port);
+    console.log('Bill McCash Slack Bot is running on port: ' + port);
 });
 
-app.post('/hello', function(req, res, next){
-    var userName = req.body.user_name;
-    var dolar = '0.0';
-    var text = '';
+// call bill
+app.post('/bill', function(req, res, next){
+    var user = req.body.user_name;
+    var difference, text;
 
     request({
         url: apiUrl,
         json: true
     }, function(error, response, body){
         if (!error && response.statusCode === 200) {
-            var value = body.split(',');
-            dolar = value[1];
+            var values = body.split(',');
+            difference = values[1];
 
-            if (userName == 'dbpolito') {
-                text = 'Hey there, boss! Eager to get your hands on some dollars, eh? Alright, the USD exchange rate to BRL today is ' + dolar + '. One day I will be as rich as you are!';
+            if (user == 'dbpolito' || user == 'hideo') {
+                text = 'Hey there, boss! Eager to get your hands on some dollars, eh? Alright, the ' + currency.from + ' exchange rate to ' + currency.to + ' today is ' + difference + '. One day I will be as rich as you are!';
             } else {
-                text = 'Hello there, fellow capitalist ' + userName + '! As far as my stock trader contact tells me, today the USD exchange rate to BRL is ' + dolar + ' !';
+                text = 'Hello there, fellow capitalist ' + user + '! As far as my stock trader contact tells me, today the ' + currency.from + ' exchange rate to ' + currency.to + ' is ' + difference + ' !';
             }
+        } else {
+            text = 'Something went wrong.';
+        }
 
-            var botPayload = {
+        if (user !== 'slackbot') {
+            return res.status(200).json({
                 text: text,
-            };
-
-            if (userName !== 'slackbot') {
-                return res.status(200).json(botPayload);
-            } else {
-                return res.status(200).end();
-            }
+            });
+        } else {
+            return res.status(200).end();
         }
     });
 });
