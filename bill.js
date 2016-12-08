759648ea2a1c1fa1ea0fd735e7e7d9fc59ffb35b
Gabriel Oliveira <gab.oliveira@outlook.com>
2016-12-08 13:15:33 -0200

    Adding Fallback to Yahoo! API

--- bill.js
+++ bill.js
@@ -17,6 +17,46 @@ const slackHook = restify.createJsonClient({url: env.slackWebhookURL});
 const dolarAPI = restify.createJsonClient({
     url: `https://economia.awesomeapi.com.br/json/${env.currency.from}-${env.currency.to}/1`
 });
+const fallbackDolarAPI = restify.createJsonClient({
+    url: `http://download.finance.yahoo.com/d/quotes.csv?e=.csv&f=sl1d1t1&s=${env.currency.from}${env.currency.to}=X`
+});
+
+const getExchangeRate = () =>{
+    return new Promise((resolve, reject) => {
+        dolarAPI.get('', (dolarErr, dolarReq, dolarRes, obj) => {
+            let tryFallback = false;
+            if(dolarErr) {
+                tryFallback =  true;
+            } else if(obj && obj[0] && obj[0].bid) {
+                resolve(obj[0].bid);
+            } else {
+                tryFallback =  true;
+            }
+            if(tryFallback) {
+                getExchangeRateFallback().then(resolve, reject);
+            }
+        });
+    });
+};
+
+const getExchangeRateFallback = () => {
+    return new Promise((resolve, reject) => {
+        fallbackDolarAPI.get('', (fallbackErr, fallbackReq, fallbackRes, fallbackObj) => {
+            if(fallbackErr) {
+                reject(fallbackErr);
+            } else if(fallbackRes && fallbackRes.body) {
+                let arr = fallbackRes.body.split(',');
+                if(arr.length > 1) {
+                    resolve(arr[1]);
+                } else {
+                    reject('Object invalid');
+                }
+            } else {
+                reject('No object received');
+            }
+        });
+    });
+};
 
 // Server
 const server = restify.createServer({
@@ -31,17 +71,17 @@ server.post(`/${env.trigger}`, (req, res, next) => {
     let user = req.params.user_name;
     let msg = env.bosses.indexOf(user) != -1 ? env.msg.boss : env.msg.fellow;
 
-    dolarAPI.get('', (dolarErr, dolarReq, dolarRes, obj) => {
-        let text = messageReplace(msg, {
-            'user': user,
-            'from': env.currency.from,
-            'to': env.currency.to,
-            'value': obj[0].bid
-        });
-
-        // send the message to slack
-        res.send({text});
-    });
+    getExchangeRate().then(
+        (exchangeRate) =>
+            res.send({text: messageReplace(msg, {
+                    'user': user,
+                    'from': env.currency.from,
+                    'to': env.currency.to,
+                    'value': exchangeRate
+                })
+            }),
+        (error) => res.send({text: env.msg.error})
+    );
 
     next();
 });
@@ -50,7 +90,7 @@ server.post(`/${env.trigger}`, (req, res, next) => {
 server.listen(port, () => console.log(`${server.name} listening at ${server.url}`));
 
 // check if cron is enabled
-if (! env.cron.enabled) {
+if (!env.cron.enabled) {
     return;
 }
 
@@ -59,15 +99,17 @@ schedule.scheduleJob(env.cron.schedule, () => {
     let currentHours = new Date().getHours();
     let msg = currentHours < 12 ? env.msg.morning : env.msg.afternoon;
 
-    dolarAPI.get('', (err, req, res, obj) => {
-        let text = messageReplace(msg, {
-            'from': env.currency.from,
-            'to': env.currency.to,
-            'value': obj[0].bid
-        });
-
-        slackHook.post('', {text}, (err, req, res, obj) => {
-            console.log(`Message sent. Status code -> ${res.statusCode}`);
-        });
-    });
+    getExchangeRate().then(
+        (exchangeRate) => {
+            let text = messageReplace(msg, {
+                'from': env.currency.from,
+                'to': env.currency.to,
+                'value': obj[0].bid
+            });
+            slackHook.post('', {text}, (err, req, res, obj) => {
+                console.log(`Message sent. Status code -> ${res.statusCode}`);
+            });
+        },
+        (error) => res.send({text: env.msg.error})
+    );
 });
