1de151a0cdef8fdf925c45ed3756e06f453c79ba
Gabriel Oliveira <gabriel.oliveira@fireworkweb.com>
2017-08-01 18:44:58 -0300

    Rewriting Bill and merging with Ellen

---
+++ src/cryptocurrency.js
@@ -0,0 +1,22 @@
+import _get from 'lodash.get'
+import axios from 'axios'
+import env from './env'
+
+const apiUrl = 'https://api.cryptonator.com/api/ticker'
+
+function fetchRate (currency) {
+    return axios.get(`${apiUrl}/${currency}-${env.cryptoCurrencies.to}`)
+}
+
+function parseResponse (response) {
+    return {
+        from: _get(response, 'data.ticker.base'),
+        to: _get(response, 'data.ticker.target'),
+        value: _get(response, 'data.ticker.price'),
+    }
+}
+
+export default function fetchCryptoCurrency (currencyToFetch) {
+    return fetchRate(currencyToFetch)
+        .then(parseResponse)
+}
---
+++ src/currency.js
@@ -0,0 +1,53 @@
+import _get from 'lodash.get'
+import axios from 'axios'
+import env from './env'
+
+const apiURL = 'http://api.fixer.io/latest'
+const apiFallbackURL = 'https://economia.awesomeapi.com.br/json/'
+
+let fetch = {
+    main: {
+        get (currency) {
+            return axios.get(apiURL, {
+                params: {
+                    base: currency,
+                    symbols: env.currencies.to,
+                },
+            })
+        },
+
+        parse (response) {
+            return _get(response, `data.rates.${env.currencies.to}`)
+        },
+    },
+
+    fallback: {
+        get (currency) {
+            return axios.get(`${apiFallbackURL}/${currency}-${env.currencies.to}/1`)
+        },
+
+        parse (response) {
+            return _get(response, 'data[0].bid')
+        },
+    },
+}
+
+function startFetch (currency, callbacks) {
+    return Promise.resolve(currency)
+        .then(callbacks.get)
+        .then(callbacks.parse)
+}
+
+function parseResponse (response, currency) {
+    return {
+        from: currency,
+        to: env.currencies.to,
+        value: response,
+    }
+}
+
+export default function fetchCurrency (currencyToFetch) {
+    return startFetch(currencyToFetch, fetch.main)
+        .catch(() => startFetch(currencyToFetch, fetch.fallback))
+        .then(response => parseResponse(response, currencyToFetch))
+}
