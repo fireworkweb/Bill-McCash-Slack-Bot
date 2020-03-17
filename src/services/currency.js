8bc196e6989e43b6eb819843412f4f52e3c7f190
Nikolas Abiusi <nikolasab@gmail.com>
2020-03-17 15:53:43 -0300

    refactor to make fallback the default code

--- src/services/currency.js
+++ src/services/currency.js
@@ -2,28 +2,12 @@ import _get from 'lodash.get'
 import axios from 'axios'
 import env from '../env'
 
-const apiURL = 'http://api.fixer.io/latest'
-const apiFallbackURL = 'https://economia.awesomeapi.com.br/json/'
+const apiURL = 'https://economia.awesomeapi.com.br/json'
 
 let fetch = {
-    main: {
+    fetchCurrency: {
         get (currency) {
-            return axios.get(apiURL, {
-                params: {
-                    base: currency,
-                    symbols: env.currencies.to,
-                },
-            })
-        },
-
-        parse (response) {
-            return _get(response, `data.rates.${env.currencies.to}`)
-        },
-    },
-
-    fallback: {
-        get (currency) {
-            return axios.get(`${apiFallbackURL}/${currency}-${env.currencies.to}/1`)
+            return axios.get(`${apiURL}/${currency}-${env.currencies.to}/1`)
         },
 
         parse (response) {
@@ -47,7 +31,7 @@ function parseResponse (response, currency) {
 }
 
 export default function fetchCurrency (currencyToFetch) {
-    return startFetch(currencyToFetch, fetch.main)
-        .catch(() => startFetch(currencyToFetch, fetch.fallback))
+    return startFetch(currencyToFetch, fetch.fetchCurrency)
+        .catch(e => console.log(e))
         .then(response => parseResponse(response, currencyToFetch))
 }
